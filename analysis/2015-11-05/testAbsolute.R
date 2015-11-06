904319bec07a9d0eaf52cccc8186966831b928fb
Sara JC Gosline <sara.gosline@sagebase.org>
2015-11-06 09:48:32 -0800

    Updated test with bug fix

--- analysis/2015-11-05/testAbsolute.R
+++ analysis/2015-11-05/testAbsolute.R
@@ -24,19 +24,20 @@ run_abs<-function(fname){
 
 
     oname=gsub('.seg','_dermalNFSample',fname)
-    sname<-paste(annots[match(gsub('X','3086-PBK-',gsub('.seg','_Final.csv',fname)),annots$File),c(2,4)],collapse=' ')
+    sname<-paste(annots[match(gsub('X','3096-PBK-',gsub('.seg','_Final.csv',fname)),annots$File),c(2,4)],collapse=' ')
 
     ##this doesn't run, file is off. let's try running hapseg instead
     RunAbsolute(fname,sample.name=sname,min.ploidy=min.ploidy,max.ploidy=max.ploidy,
                 max.sigma.h=max.sigma.h,platform='SNP_6.0',copy_num_type='total',
                 min.mut.af=0,sigma.p=sigma.p,results.dir='abs_res',verbose=TRUE,
-                output.fn.base=sname,primary.disease='PNF',max.as.seg.count=max.as.seg.count,
+                output.fn.base=oname,primary.disease='PNF',max.as.seg.count=max.as.seg.count,
                 max.non.clonal = max.non.clonal, max.neg.genome=max.neg.genome)
 }
 
+require(parallel)
 
-sapply(unique(segs$ID),function(x){
+mclapply(unique(segs$ID),function(x){
     fname=paste(x,'seg',sep='.')
     write.table(segs[which(segs$ID==x),-1],file=paste(x,'seg',sep='.'),sep='\t')
     run_abs(fname)
-})
+},mc.cores=4)
