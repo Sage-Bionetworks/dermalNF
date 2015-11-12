913338a5ad2487a0e02559f9eb1c21c126188367
Sara JC Gosline <sara.gosline@sagebase.org>
2015-11-12 13:37:37 -0800

    Added text

---
+++ analysis/2015-11-12/mergeAllVcfs.py
@@ -0,0 +1,19 @@
+'''
+VCFs should be merged by sample
+'''
+import sys,os,re
+sys.path.append('../../bin/')
+import  wgsVariantRefinement as wgs
+
+##get all synfile paths
+synfiles=[wgs.syn.get(si).path+'\n' for si in wgs.syn_ids]
+open('allVcfFiles.txt','w').writelines(synfiles)
+
+#now call bcftools to merge into large vcf
+vcf='allDermalNFSamples.vcf'
+
+cmd='bcftools merge -o %s -l %s --use-header %s'%(vcf,'allVcfFiles.txt',synfiles[0])
+
+os.system(cmd)
+#now call vqsr?
+#res=wgs.snp_calibrate_model(vcf)
