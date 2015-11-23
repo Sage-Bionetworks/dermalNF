ff2888bf547329b921edae3108688da1ef854efe
Sara JC Gosline <sara.gosline@sagebase.org>
2015-11-23 12:53:16 -0800

    updated

--- analysis/2015-11-18/randomForestRNAtest.R
+++ analysis/2015-11-18/randomForestRNAtest.R
@@ -12,3 +12,9 @@ names(pats)<-rna.annot$synapseId
 df<-data.frame(Patients=as.factor(pats[colnames(rna)]),t(rna))
 
 rf<-randomForest(Patients ~. , data=df)
+
+library(pheatmap)
+pheatmap(log10(0.001+rna[rownames(rf$importance)[order(rf$importance,decreasing=T)[1:100]],]),annotation_col=data.frame(Patient=pats),cellheight=10,cellwidth=10,file='top100RFpredictingGenes.png')
+
+nzvals=rf$importance[which(rf$importance>0),]
+write(names(nzvals)[order(nzvals,decreasing=T)],file='patientPredictiv')
