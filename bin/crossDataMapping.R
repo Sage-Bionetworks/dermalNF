c81f87c7604d646170fc552f228a2a47b4e5959e
Sara JC Gosline <sara.gosline@sagebase.org>
2015-12-01 16:48:34 -0800

    Updated cross data comparison files to use new table

--- bin/crossDataMapping.R
+++ bin/crossDataMapping.R
@@ -12,7 +12,7 @@ source('../../bin/dermalNFData.R')
 
 getPatientMapping<-function(patientId=NA){
     #here we can get the patient mappings from the synapse talbe
-    schemaId='syn5478651'
+    schemaId='syn5479878'
     if(is.na(patientId))
         queryResult<-synTableQuery(sprintf("select * from %s", schemaId))
     else
@@ -26,7 +26,7 @@ storePatientMappingDataInTable<-function(){
                                         #a synapse table
 
                                         #first get all the annotations
-    rna.map<-rna_bam_annotations()
+    rna.map<-rna_annotations()
     cnv.map<-cnv_annotations()
     wgs.map<-wgs_annotations()
     prot.map<-protein_annotations()
@@ -40,34 +40,35 @@ storePatientMappingDataInTable<-function(){
     sample.list<-NULL
                                         #patient.sample.matches<-sapply(all.patients,function(x){\
     for(x in all.patients){
+      print(paste('Collecting samples for patient',x))
         #TODO: harmonize identifiers across all...
-        rna.samps<-unique(rna.map[grep(paste("CT0+",x,'$',sep=''),rna.map$patientID),])
-        dna.samps<-unique(wgs.map[grep(paste("CT0+",x,'$',sep=''),wgs.map$patientID),])
+        rna.samps<-unique(rna.map[grep(paste("CT0+",x,'$',sep=''),rna.map$patientId),])
+        dna.samps<-unique(wgs.map[grep(paste("CT0+",x,'$',sep=''),wgs.map$patientId),])
         prot.samps<-unique(prot.map[grep(paste("CT0+",x,'$',sep=''),prot.map$patientId),])
-        cnv.samps<-unique(cnv.map[grep(paste("CT0+",x,'$',sep=''),cnv.map$patientID),])
+        cnv.samps<-unique(cnv.map[grep(paste("CT0+",x,'$',sep=''),cnv.map$patientId),])
 
         ##add in all samples that match
-        matched.rna<-intersect(rna.samps$tissueID,dna.samps$alternateTumorID)
+        matched.rna<-intersect(rna.samps$tissueId,dna.samps$alternateTumorId)
         print(paste('Found',length(matched.rna),'RNA samples that match dna samples for patient',x))
         pcount=1
         for(mr in matched.rna){
-            dna.tiss=dna.samps[match(mr,dna.samps$alternateTumorID),'tissueID']
-            wgs.si=dna.samps[match(mr,dna.samps$alternateTumorID),'synapseId']
+            dna.tiss=dna.samps[match(mr,dna.samps$alternateTumorId),'tissueId']
+            wgs.si=dna.samps[match(mr,dna.samps$alternateTumorId),'synapseId']
             prot.si<-prot.samps[match(dna.tiss,prot.samps$tissueId),'synapseId']
-            cnv.si<-cnv.samps[match(dna.tiss,cnv.samps$tissueID),'synapseID']
-            rna.si<-rna.samps[match(mr,rna.samps$tissueID),'synapseID']
+            cnv.si<-cnv.samps[match(dna.tiss,cnv.samps$tissueId),'synapseId']
+            rna.si<-rna.samps[match(mr,rna.samps$tissueId),'synapseId']
                                         # sample.list[[paste(x,pcount,sep='_')]]<-
             sample.list<-rbind(sample.list,list(patient=x,DnaID=dna.tiss,RnaID=mr,RNASeq=rna.si,WGS=wgs.si,Prot=prot.si,CNV=cnv.si))
             pcount<-pcount+1
         }
 
         #next add in unmatched dna
-        unmatched.dna<-setdiff(dna.samps$tissueID,rna.samps$alternateTumorID)
+        unmatched.dna<-setdiff(dna.samps$tissueId,rna.samps$alternateTumorId)
         for(mr in unmatched.dna){
             dna.tiss=mr
-            wgs.si=dna.samps[match(dna.tiss,dna.samps$tissueID),'synapseId']
+            wgs.si=dna.samps[match(dna.tiss,dna.samps$tissueId),'synapseId']
             prot.si<-prot.samps[match(dna.tiss,prot.samps$tissueId),'synapseId']
-            cnv.si<-cnv.samps[match(dna.tiss,cnv.samps$tissueID),'synapseID']
+            cnv.si<-cnv.samps[match(dna.tiss,cnv.samps$tissueId),'synapseId']
 
             rna.si<-NA
                                         #      sample.list[[paste(x,pcount,sep='_')]]<-
@@ -76,12 +77,12 @@ storePatientMappingDataInTable<-function(){
         }
 
         ##then add in unamtched rna
-        unmatched.rna<-setdiff(rna.samps$tissueID,dna.samps$alternateTumorID)
+        unmatched.rna<-setdiff(rna.samps$tissueId,dna.samps$alternateTumorId)
         for(mr in unmatched.rna){
             wgs.si=NA
             prot.si<-NA
             cnv.si<-NA
-            rna.si<-rna.samps[which(rna.samps$tissueID==mr),'synapseID']
+            rna.si<-rna.samps[which(rna.samps$tissueId==mr),'synapseId']
 
                                         #      sample.list[[paste(x,pcount,sep='_')]]<-
             sample.list<-rbind(sample.list,list(patient=x,DnaID=NA,RnaID=mr,RNASeq=rna.si,WGS=wgs.si,Prot=prot.si,CNV=cnv.si))
