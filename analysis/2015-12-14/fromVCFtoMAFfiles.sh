415a897a1723bc91a392de2c28ff27b0542e79e7
Sara JC Gosline <sara.gosline@sagebase.org>
2015-12-14 15:47:21 -0800

    Updated new vcf file to execute.

--- analysis/2015-12-14/fromVCFtoMAFfiles.sh
+++ analysis/2015-12-14/fromVCFtoMAFfiles.sh
@@ -7,5 +7,16 @@ cmd='python ../../bin/wgsVariantRefinement.py --vcf $merged_vcf --libDir $libdir
 echo $cmd
 $cmd
 
-newvcf=''
+zc="bgzip recalibrated_indels_for_recalibrated_snps_for_dermalNFall.vcf"
+echo $zc
+$zc
 
+newvcf=recalibrated_indels_for_recalibrated_snps_for_dermalNFall.vcf.gz
+
+syncmd="synapse store $newvcf --parentId syn5522790"
+echo $syncmd
+$syncmd
+
+mafcmd="python ../../bin/vcfDataProcessing.py"
+echo $mafcmd
+$mafcmd
