55f6727abed990b769aa2e4cfa8d996240773320
Sara JC Gosline <sara.gosline@sagebase.org>
2016-02-09 19:59:55 -0800

    updated with new plo

---
+++ analysis/2016-02-09/figure_4_5_draft.R
@@ -0,0 +1,118 @@
+##begin to work on figures
+
+source("../../bin/dermalNFData.R")
+
+##figure 4 is the easiest, starting wtiht hat
+
+library(ggplot2)
+library(dplyr)
+annotes=rna_annotations()
+
+
+rna_counts=rna_count_matrix(doLogNorm=TRUE)
+
+res=tidyr::gather(as.data.frame(rna_counts),key=Sample,value=LogCounts)
+res$Patient=sapply(annotes$patientId[match(res$Sample,annotes$synapseId)],function(x) gsub("CT0*",'',x))
+res$Tissue=annotes$tissueId[match(res$Sample,annotes$synapseId)]
+res$Gene=rep(rownames(rna_counts),ncol(rna_counts))
+#nres=subset(res,Gene=="NF1")
+p<-ggplot(res,aes(y=LogCounts,x=Sample))+geom_violin(aes(fill=Patient))+geom_point(data=subset(res,Gene == 'NF1'),shape=4)
+png('violinPlotOfLogNormDeseqCounts.png',width=800)
+print(p)
+dev.off()
+
+rna_counts=rna_count_matrix(doLogNorm=TRUE,minCount=2)
+
+res=tidyr::gather(as.data.frame(rna_counts),key=Sample,value=LogCounts)
+res$Patient=sapply(annotes$patientId[match(res$Sample,annotes$synapseId)],function(x) gsub("CT0*",'',x))
+res$Tissue=annotes$tissueId[match(res$Sample,annotes$synapseId)]
+res$Gene=rep(rownames(rna_counts),ncol(rna_counts))
+#nres=subset(res,Gene=="NF1")
+p<-ggplot(res,aes(y=LogCounts,x=Sample))+geom_boxplot(aes(fill=Patient))+geom_point(data=subset(res,Gene == 'NF1'),shape=4)
+png('boxPlotOfLogNormMinCount2DeseqCounts.png',width=800)
+print(p)
+dev.off()
+
+##now do regular normalization
+rna_counts=rna_count_matrix(doNorm=TRUE)
+
+res=tidyr::gather(as.data.frame(rna_counts),key=Sample,value=NormCounts)
+res$Patient=sapply(annotes$patientId[match(res$Sample,annotes$synapseId)],function(x) gsub("CT0*",'',x))
+res$Tissue=annotes$tissueId[match(res$Sample,annotes$synapseId)]
+res$Gene=rep(rownames(rna_counts),ncol(rna_counts))
+#nres=subset(res,Gene=="NF1")
+p<-ggplot(res,aes(y=NormCounts,x=Sample))+geom_violin(aes(fill=Patient))+geom_point(data=subset(res,Gene == 'NF1'),shape=4)+scale_y_log10()
+png('violinPlotOfSizeFactorNormDeseqCounts.png',width=800)
+print(p)
+dev.off()
+
+res$NormCounts=log2(res$NormCounts+1)
+p<-ggplot(res,aes(y=NormCounts,x=Sample))+geom_violin(aes(fill=Patient))+geom_point(data=subset(res,Gene == 'NF1'),shape=4)
+png('violinPlotOfLogSizeFactorNormDeseqCounts.png',width=800)
+print(p)
+dev.off()
+
+rna_counts=rna_count_matrix(doNorm=TRUE,minCount=2)
+
+res=tidyr::gather(as.data.frame(rna_counts),key=Sample,value=NormCounts)
+res$Patient=sapply(annotes$patientId[match(res$Sample,annotes$synapseId)],function(x) gsub("CT0*",'',x))
+res$Tissue=annotes$tissueId[match(res$Sample,annotes$synapseId)]
+res$Gene=rep(rownames(rna_counts),ncol(rna_counts))
+#nres=subset(res,Gene=="NF1")
+p<-ggplot(res,aes(y=NormCounts,x=Sample))+geom_boxplot(aes(fill=Patient))+geom_point(data=subset(res,Gene == 'NF1'),shape=4)+scale_y_log10()
+png('boxPlotOfSizeFactorNormDeseqCounts.png',width=800)
+print(p)
+dev.off()
+
+res$NormCounts=log2(res$NormCounts+1)
+p<-ggplot(res,aes(y=NormCounts,x=Sample))+geom_boxplot(aes(fill=Patient))+geom_point(data=subset(res,Gene == 'NF1'),shape=4)
+png('boxPlotOfLogSizeFactorNormMinCount2DeseqCounts.png',width=800)
+print(p)
+dev.off()
+
+
+fpkm_annotes=synapseQuery("select sampleID,patientID,tissueID from entity where parentId=='syn5492805'")
+
+rna_fpkm=rna_fpkm_matrix(FALSE)
+samples<-sapply(colnames(rna_fpkm),function(x) gsub('^X','',gsub('.','-',x,fixed=T)))
+colnames(rna_fpkm)<-samples
+res=tidyr::gather(as.data.frame(rna_fpkm),key=Sample,value=FPKM)
+#samples<-sapply(res$Sample,function(x) paste(gsub('^X','',gsub('.','_',x,fixed=T)),'_Final.csv',sep=''))
+res$Patient=sapply(fpkm_annotes$entity.patientID[match(res$Sample,fpkm_annotes$entity.sampleID)],function(x) gsub("CT0*",'',x))
+res$Tissue=fpkm_annotes$entity.tissueID[match(res$Sample,fpkm_annotes$entity.sampleID)]
+res$Gene=rep(rownames(rna_fpkm),ncol(rna_fpkm))
+#nres=subset(res,Gene=="NF1")
+p<-ggplot(res,aes(y=FPKM+1,x=Sample))+geom_violin(aes(fill=Patient))+geom_point(data=subset(res,Gene == 'NF1'),shape=4)+scale_y_log10()
+png('violinPlotOfFpkm.png',width=800)
+print(p)
+dev.off()
+
+fres=subset(res,FPKM>0.1)
+p<-ggplot(res,aes(y=FPKM+1,x=Sample))+geom_boxplot(aes(fill=Patient))+geom_point(data=subset(res,Gene == 'NF1'),shape=4)+scale_y_log10()
+png('boxPlotOfFpkmMin0.1.png',width=800)
+print(p)
+dev.off()
+
+prot_annote=protein_annotations()
+allprots=prot_normalized()
+
+pres=tidyr::gather(as.data.frame(allprots[,-1],key=Sample,value=FoldChange))
+colnames(pres)<-c("Sample","FoldChange")
+pres$Protein=rep(allprots[,1],ncol(allprots)-1)
+pres$Patient=sapply(prot_annote$patientId[match(pres$Sample,prot_annote$synapseId)],function(x) gsub("CT0+","",x))
+p<-ggplot(pres,aes(y=FoldChange,x=Sample))+geom_boxplot(aes(fill=Patient))+scale_y_log10()
+png('boxPlotOfProteins.png',width=800)
+print(p)
+dev.off()
+
+p<-ggplot(pres,aes(y=FoldChange,x=Sample))+geom_violine(aes(fill=Patient))+scale_y_log10()
+png('violinPlotOfProteins.png',width=800)
+print(p)
+dev.off()
+
+
+
+
+
+
+
