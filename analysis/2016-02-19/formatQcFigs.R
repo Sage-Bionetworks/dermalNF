2ea9ef7371cbad9e6da02e59549f494cec8004e4
Sara JC Gosline <sgosline@mit.edu>
2016-02-19 08:55:51 -0800

    updated

---
+++ analysis/2016-02-19/formatQcFigs.R
@@ -0,0 +1,31 @@
+#'
+#' Quality control figures
+#' 
+#' 
+#' 
+source('../../bin/dermalNFData.R')
+
+#' Figure 1- cluster of CNV data
+#' alongside NF1 region
+makeCNVClusterPlot<-function(){
+  
+  
+}
+
+#' Figure 2- cluster of WGS data
+#' panel a and b are processing of VCF cluster
+makeWGSClusters<-function(){
+  
+  
+}
+
+#' Figure 3- RNA-seq read distribution plots
+makeRNASeqCountDist<-function(){
+  
+}
+
+
+#' Figure 4- Proteomics clustering by run?
+makeProteomicsFigs<-function(){
+  
+}
