f3ce1c4399f42355e228b3d386578a6045784c20
Sara JC Gosline <sara.gosline@sagebase.org>
2016-04-13 15:54:24 -0700

    updated with bam command, also need to update with mutect update

--- analysis/2016-04-13/runMuTectOnSamps.py
+++ analysis/2016-04-13/runMuTectOnSamps.py
@@ -13,20 +13,29 @@ syn.login()
 stcommand='samtools mpileup -f ../../../lib/ucsc.hg19.fasta '
 vscommand='java -r ~/VarScan.v2.3.9.jar pileup2snp'
 
-def runMutect(normfile,tumfile,out_prefix):
+def runMutect(normfile,tumfile,out_prefix,cmdfile=''):
     reference='../../lib/ucsc.hg19.fasta'
     cosmic='../../lib/sorted_b37_cosmic_v54_120711_withchr.vcf'
     dbsnp='../../lib/sorted_dbsnp_138.hg19.vcf'
     intervals='../../lib/ucsc.hg19.dict'
     cmd='java -jar ~/muTect/muTect-1.1.4.jar --analysis_type MuTect --reference_sequence %s --cosmic %s \
          --dbsnp %s --input_file:normal %s --input_file:tumor %s --out %s -cov %s'%(reference,cosmic,dbsnp,normfile,tumfile,out_prefix+'.out',out_prefix+'_coverage.wig.txt')
+    pstr='Running muTectv2 on %s and %s'%(normfile,tumfile)
+    if cmdfile=='':
+        print pstr
+
+        os.system(cmd)
+
+    else:
+        cmdfile.write('echo "'+pstr+'"\n'+cmd+'\n')
+
     print cmd
 
 
-def updateBams(bamfile,cmdfile=''): 
-'''
+def updateBams(bamfile,cmdfile=''):
+    '''
     The BAM files require adding read groups, re-ordering, and indexing before they can be run by MuTect
-'''
+    '''
     outfile=re.sub('.bam','_rg.bam',bamfile)
     picmd='java -jar ~/picard-tools-2.1.1/picard.jar AddOrReplaceReadGroups \
            I=%s O=%s RGID=1 RGLB=hg19 RGPL=illumina RGPU=dragen RGSM=%s'%(bamfile,outfile,re.sub('.bam','',bamfile))
@@ -42,19 +51,22 @@ def updateBams(bamfile,cmdfile=''):
     pic2cmd='java -jar ~/picard-tools-2.1.1/picard.jar ReorderSam INPUT=%s OUTPUT=%s REFERENCE=%s'%(outfile,ordered,'../../lib/ucsc.hg19.fasta')
     if not os.path.exists(ordered):
 	ustr='Re-ordering BAM file to make %s'%(ordered)
-	if cmdfile=='':	
+	if cmdfile=='':
 	    print ustr
 	    os.system(pic2cmd)
 	else:
-	    cmdfile.write('echo "'+ustr+'"\n'+cmdfile+'\n')
+	    cmdfile.write('echo "'+ustr+'"\n'+pic2cmd+'\n')
+
 
     ind='samtools index %s'%(ordered)
     ustr='Running samtools to index %s'%(ordered)
     if cmdfile=='':
-	print ustr
-	os.system(ind)
+        print ustr
+        os.system(ind)
     else:
-	cmdfile.write('echo "'+ustr+'"\n'+ind+'\n')
+        cmdfile.write('echo "'+ustr+'"\n'+ind+'\n')
+
+
     return ordered
 
 
@@ -67,11 +79,11 @@ def getBamPath(synid,cmdfile=''):
     awscmd='aws s3 cp %s %s'%(os.path.join(bamrepo,bamf),bamf)
     print awscmd
     if not os.path.exists(bamf):
-	if cmdfile=='':	
+	if cmdfile=='':
     	    os.system(awscmd)
 	else:
 	    cmdfile.write('echo "Getting %s from AWS"\n%s\n'%(bamf,awscmd))
-    
+
     return bamf
 
 allpats=['1','2','3','4','5','6','8','9','11']
@@ -83,7 +95,9 @@ for p in allpats:
     if len(normind)==0:
         print 'No normal file found for patient '+p
         next
-    normfile=updateBams(getBamPath(normind[0]))
+    normscript=open('patient'+p+'_'+normind[0]+'bamProcessing.sh','w')
+    normfile=updateBams(getBamPath(normind[0],normscript),normscript)
+    normscript.close()
 
     tuminds=[df['WGS'][ind] for ind,a in enumerate(df['DnaID']) if (a!='PBMC' and not math.isnan(float(a)))]
     tuminds=tuminds[0:1]
@@ -91,9 +105,8 @@ for p in allpats:
         #run pileup command
         cmdfile=open(tu+'cmd.sh','w')
         cmdfile.write('#/bin/bash\n')
-        bf=updateBams(getBamPath(tu),cmdfile)
+        bf=updateBams(getBamPath(tu,cmdfile),cmdfile)
         outfile='patient_%s_tumor_%s_vs_normal_%s.snp'%(p,tu,normind[0])
         print outfile
         cmd = runMutect(normfile,bf,outfile,cmdfile)
 	cmdfile.close()
-
