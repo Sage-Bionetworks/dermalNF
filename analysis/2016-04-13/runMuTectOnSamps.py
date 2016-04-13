eb9a07aaadd9d6f0ea51b131f21bf250e2d1e82f
Ubuntu <ubuntu@ip-172-31-55-243.ec2.internal>
2016-04-13 23:48:34 +0000

    Updated with java updates and more background of changes

--- analysis/2016-04-13/runMuTectOnSamps.py
+++ analysis/2016-04-13/runMuTectOnSamps.py
@@ -17,19 +17,30 @@ def runMutect(normfile,tumfile,out_prefix,cmdfile=''):
     reference='../../lib/ucsc.hg19.fasta'
     cosmic='../../lib/sorted_b37_cosmic_v54_120711_withchr.vcf'
     dbsnp='../../lib/sorted_dbsnp_138.hg19.vcf'
-    intervals='../../lib/ucsc.hg19.dict'
-    cmd='java -jar ~/muTect/muTect-1.1.4.jar --analysis_type MuTect --reference_sequence %s --cosmic %s \
-         --dbsnp %s --input_file:normal %s --input_file:tumor %s --out %s -cov %s'%(reference,cosmic,dbsnp,normfile,tumfile,out_prefix+'.out',out_prefix+'_coverage.wig.txt')
-    pstr='Running muTectv2 on %s and %s'%(normfile,tumfile)
+    intervals=['chr1','chr2','chr3','chr4','chr6','chr6','chr7','chr8','chr9','chr10','chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19','chr20','chr21','chr22','chrX','chrY','chrM']#'../../lib/ucsc.hg19.dict'
     if cmdfile=='':
-        print pstr
-
-        os.system(cmd)
-
+	os.system('sudo apt-get -f install oracle-java7-set-default')
     else:
-        cmdfile.write('echo "'+pstr+'"\n'+cmd+'\n')
+	cmdfile.write('sudo apt-get -f install oracle-java7-set-default\n') 
+    for iv in intervals:
+	cmd='java -jar ~/GenomeAnalysisTK.jar --analysis_type MuTect2 --reference_sequence %s --input_file:normal %s\
+	 --input_file:tumor %s --out %s --interval %s'%(reference,normfile,tumfile,out_prefix+'_'+iv+'.vcf',iv)
 
-    print cmd
+   # cmd='java -jar ~/muTect/muTect-1.1.4.jar --analysis_type MuTect --reference_sequence %s --cosmic %s \
+   #      --dbsnp %s --input_file:normal %s --input_file:tumor %s --out %s -cov %s'%(reference,cosmic,dbsnp,normfile,tumfile,out_prefix+'.out',out_prefix+'_coverage.wig.txt')
+    	pstr='Running muTectv2 on %s and %s on region %s'%(normfile,tumfile,iv)
+   	if cmdfile=='':
+       	    print pstr
+      	    os.system(cmd)
+
+    	else:
+            cmdfile.write('echo "'+pstr+'"\n'+cmd+'\n')
+    allfiles=out_prefix+'_chr*.vcf'
+    if cmdfile=='':
+	os.system('cat '+allfiles+'>'+out_prefix+'.vcf')
+	os.system('rm '+allfiles)
+    else:
+	cmdfile.write('cat '+allfiles+'>'+out_prefix+'.vcf\nrm '+allfiles)
 
 
 def updateBams(bamfile,cmdfile=''):
@@ -37,16 +48,16 @@ def updateBams(bamfile,cmdfile=''):
     The BAM files require adding read groups, re-ordering, and indexing before they can be run by MuTect
     '''
     outfile=re.sub('.bam','_rg.bam',bamfile)
-    picmd='java -jar ~/picard-tools-2.1.1/picard.jar AddOrReplaceReadGroups \
+    picmd='sudo apt-get -f install oracle-java8-set-default\njava -jar ~/picard-tools-2.1.1/picard.jar AddOrReplaceReadGroups \
            I=%s O=%s RGID=1 RGLB=hg19 RGPL=illumina RGPU=dragen RGSM=%s'%(bamfile,outfile,re.sub('.bam','',bamfile))
-    print picmd
+#    print picmd
     if not os.path.exists(outfile):
     	if cmdfile=='':
 	    print 'Adding read groups to make %s'%(outfile)
 	    os.system(picmd)
 	else:
 	    cmdfile.write('echo "Adding read groups to make %s"\n%s\n'%(outfile,picmd))
-
+	    cmdfile.write('rm '+bamfile+'\n')
     ordered=re.sub('.bam','_ordered.bam',outfile)
     pic2cmd='java -jar ~/picard-tools-2.1.1/picard.jar ReorderSam INPUT=%s OUTPUT=%s REFERENCE=%s'%(outfile,ordered,'../../lib/ucsc.hg19.fasta')
     if not os.path.exists(ordered):
@@ -55,7 +66,7 @@ def updateBams(bamfile,cmdfile=''):
 	    print ustr
 	    os.system(pic2cmd)
 	else:
-	    cmdfile.write('echo "'+ustr+'"\n'+pic2cmd+'\n')
+	    cmdfile.write('echo "'+ustr+'"\n'+pic2cmd+'\nrm '+outfile+'\n')
 
 
     ind='samtools index %s'%(ordered)
@@ -77,7 +88,7 @@ def getBamPath(synid,cmdfile=''):
     f=syn.get(synid,downloadFile=False).name
     bamf=re.sub(".vcf",'.bam',f)
     awscmd='aws s3 cp %s %s'%(os.path.join(bamrepo,bamf),bamf)
-    print awscmd
+ #   print awscmd
     if not os.path.exists(bamf):
 	if cmdfile=='':
     	    os.system(awscmd)
@@ -87,7 +98,6 @@ def getBamPath(synid,cmdfile=''):
     return bamf
 
 allpats=['1','2','3','4','5','6','8','9','11']
-allpats = ['1']
 for p in allpats:
     res=syn.tableQuery("SELECT DnaID,WGS FROM syn5556216 where Patient=%s"%(p))
     df=res.asDataFrame()
@@ -100,13 +110,16 @@ for p in allpats:
     normscript.close()
 
     tuminds=[df['WGS'][ind] for ind,a in enumerate(df['DnaID']) if (a!='PBMC' and not math.isnan(float(a)))]
-    tuminds=tuminds[0:1]
+#    tuminds=tuminds[0:1]
+    print tuminds
     for tu in tuminds:
-        #run pileup command
-        cmdfile=open(tu+'cmd.sh','w')
+	if isinstance(tu,float):
+	    continue 
+      #run pileup command
+	cmdfile=open(tu+'cmd.sh','w')
         cmdfile.write('#/bin/bash\n')
         bf=updateBams(getBamPath(tu,cmdfile),cmdfile)
         outfile='patient_%s_tumor_%s_vs_normal_%s.snp'%(p,tu,normind[0])
-        print outfile
+    #    print outfile
         cmd = runMutect(normfile,bf,outfile,cmdfile)
 	cmdfile.close()
