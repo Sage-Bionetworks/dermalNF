88f8f07932b6ffd02bcfac62d2fc8561b136b91d
Sara JC Gosline <sara.gosline@sagebase.org>
2016-09-14 10:39:25 -0700

    Debugged file a bit

--- analysis/2016-09-14/reProcessOriginalFiles.R
+++ analysis/2016-09-14/reProcessOriginalFiles.R
@@ -4,12 +4,13 @@ synapseLogin()
 orig.dir<-'/scratch/DERMALNF/disk1/proteomics_data'
 
 for(file in list.files(orig.dir)){
-    tab<-read.table(file,sep='\t',header=T)
+    tab<-read.table(paste(orig.dir,file,sep='/'),sep='\t',header=T)
     orig<-tab[,1:5]
     for(i in colnames(tab)[6:9]){
         nzvals<-which(tab[,1]!=0.0)
-        print(paste('found',lenth(nzvals),'non zero values for column',i))
+        print(paste('found',length(nzvals),'non zero values for column',i))
         newtab<-cbind(orig[nzvals,],tab[nzvals,i])
         fname=gsub('.txt',paste(gsub('.','_',i,fixed=T),'.txt',sep=''),file)
         write.table(newtab,file=fname)
     }
+}
